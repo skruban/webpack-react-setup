9aeae14916bc17cafc373387642453fafcfd57ef
skruban <sudhan.kantharuban@gmail.com>
2019-03-23 03:17:04 +0530

    removed previous commits

--- dev/index.js
+++
@@ -1,8 +0,0 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
-
-const App = () => {
-  return <div>Hello world!</div>
-}
-
-ReactDOM.render(<App />, document.getElementById('root'))
